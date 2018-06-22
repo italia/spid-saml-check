9c0967fbd0a14c234741fa149ef2b2491fabb767
Paolo Smiraglia <paolo.smiraglia@gmail.com>
2018-06-22 22:36:22 +0200

    Add scripts to check the XSD and the signature

---
+++ script/check-authn-request-xsd-and-signature.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+_DATA_DIR=${DATA_DIR:-"./data"}
+
+SAMLRequest=`cat ${_DATA_DIR}/SAMLRequest.b64.txt`
+RelayState=`cat ${_DATA_DIR}/RelayState.b64.txt`
+Signature=`cat ${_DATA_DIR}/Signature.b64.txt`
+SigAlg=`cat ${_DATA_DIR}/SigAlg.b64.txt`
+
+
+if [ "X${Signature}" == "X" ]; then # HTTP-POST
+
+    # decode SAMLRequest
+    req=`mktemp`
+    echo -n ${SAMLRequest} | base64 -d > ${req}
+
+    # verify against XSD
+    xmllint --noout --schema ./xsd/saml-schema-protocol-2.0.xsd ${req}
+    if [ $? -ne 0 ]; then
+        rm ${req}
+        exit 1
+    fi
+
+    # verify XML signature
+    xmlsec1 \
+        --verify \
+        --insecure \
+        --id-attr:ID urn:oasis:names:tc:SAML:2.0:protocol:AuthnRequest \
+        ${req}
+    if [ $? -ne 0 ]; then
+        rm ${req}
+        exit 1
+    fi
+    
+    rm ${req}
+else # HTTP-Redirect
+
+    # decode SAMLRequest
+    req=`mktemp`
+    echo -n ${SAMLRequest} \
+        | base64 -d \
+        | python -c "import sys, zlib;\
+                     sys.stdout.write(zlib.decompress(sys.stdin.buffer.read(), -15).decode('utf-8'))" \
+        > ${req}
+    
+    # verify against XSD
+    xmllint --noout --schema ./xsd/saml-schema-protocol-2.0.xsd ${req}
+    if [ $? -ne 0 ]; then
+        rm ${req}
+        exit 1
+    fi
+
+    # decode XML signature
+    signature=`mktemp`
+    echo -n ${Signature} | base64 -d > ${signature}
+
+    # compose signature payload
+    request=`echo -n ${SAMLRequest} \
+        | python -c "import sys, urllib.parse as p;\
+                     sys.stdout.write(p.quote_plus(sys.stdin.read()))"`
+    
+    relay_state=`echo -n ${RelayState} \
+        | python -c "import sys, urllib.parse as p;\
+                     sys.stdout.write(p.quote_plus(sys.stdin.read()))"`
+    
+    sig_alg=`echo -n ${SigAlg} \
+        | python -c "import sys, urllib.parse as p;\
+                     sys.stdout.write(p.quote_plus(sys.stdin.read()))"`
+
+    payload=`mktemp`
+    echo -n "SAMLRequest=${request}&RelayState=${relay_state}&SigAlg=${sig_alg}" > ${payload}
+
+    # verify the signature with all the "signing" certificates that were
+    # published within the metadata
+    sign_ok=0
+    for cert in `find ${_DATA_DIR} -type f -name *.signature.pem | tr '\n' ' '`; do
+        pubkey=`mktemp`
+        openssl x509 -in ${cert} -noout -pubkey > ${pubkey}
+
+        openssl dgst -verify ${pubkey} -signature ${signature} ${payload}
+        if [ $? -eq 0 ]; then
+            sign_ok=1
+        fi
+        rm ${pubkey}
+    done
+
+    if [ ${sign_ok} -ne 1 ]; then
+        rm ${payload} ${signature} ${req}
+        exit 1
+    fi
+    
+    rm ${payload} ${signature} ${req}
+fi
---
+++ script/parse-authn-request.py
@@ -0,0 +1,82 @@
+import OpenSSL
+import base64
+import lxml.objectify
+import os
+import re
+import sys
+import urllib.parse
+import zlib
+
+from io import BytesIO
+from lxml import etree as ET
+
+REQUEST = os.getenv('REQUEST', None)
+METADATA = os.getenv('METADATA', None)
+
+DATA_DIR = os.getenv('DATA_DIR', './data')
+
+# check if envvars are set
+if not REQUEST:
+    sys.exit(1)
+
+if not METADATA:
+    sys.exit(1)
+
+# parse the captured authentication request
+request = None
+with open(REQUEST, 'rb') as f:
+    request = f.read()
+    f.close()
+params = urllib.parse.parse_qs(request.decode('utf-8'))
+
+# save the authentication request parametes/fields in separate files
+for par in ['SAMLRequest', 'RelayState', 'Signature', 'SigAlg']:
+    if par in params:
+        content = params[par][0]
+    else:
+        content = ''
+
+    fname = '%s/%s.b64.txt' % (DATA_DIR, par)
+    with open(fname, 'w') as f:
+        f.write(content)
+        f.close()
+
+# if HTTP-Redirect extract the signing certificate(s) from the metadata
+if 'SigAlg' in params:
+
+    # load metadata file
+    with open(METADATA, 'rb') as md_file:
+        md = md_file.read()
+        doc = ET.parse(BytesIO(md))
+        md_file.close()
+
+    # remove the namespace to simplify XPath
+    root = doc.getroot()
+    for elem in root.getiterator():
+        if not hasattr(elem.tag, 'find'):
+            continue
+        i = elem.tag.find('}')
+        if i >= 0:
+            elem.tag = elem.tag[i+1:]
+    lxml.objectify.deannotate(root, cleanup_namespaces=True)
+
+    # save each certificate in a specific file (*.signature.pem)
+    certs = doc.xpath('//SPSSODescriptor/KeyDescriptor[@use="signing"]'
+                      '/KeyInfo/X509Data/X509Certificate')
+    for cert in certs:
+        b64 = re.sub(r'[\s]', '', cert.text)
+        pem = []
+        n = 72
+        pem.append('-----BEGIN CERTIFICATE-----')
+        [pem.append(b64[i:i+n]) for i in range(0, len(b64), n)]
+        pem.append('-----END CERTIFICATE-----')
+
+        x509 = OpenSSL.crypto.load_certificate(
+            OpenSSL.crypto.FILETYPE_ASN1,
+            base64.b64decode(b64)
+        )
+        dgst = x509.digest('sha256').decode('utf-8').replace(':', '')
+        fname = '%s/%s.signature.pem' % (DATA_DIR, dgst[0:16])
+        with open(fname, 'w') as f:
+            f.write('\n'.join(pem))
+            f.close()
