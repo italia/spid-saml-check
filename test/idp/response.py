979eebd3aa89287675b9492c5ed4d1ed4cd11e6d
Paolo Smiraglia <paolo.smiraglia@gmail.com>
2018-06-25 16:54:48 +0200

    Add a first layer of response validation

---
+++ test/idp/response.py
@@ -0,0 +1,157 @@
+import base64
+import lxml.objectify
+import os
+import re
+import unittest
+import urllib.parse
+import validators
+
+from io import BytesIO
+from lxml import etree as ET
+
+RESPONSE = os.getenv('RESPONSE', None)
+DATA_DIR = os.getenv('DATA_DIR', './data')
+
+_RE_UTC = r'^\d{4}-\d{2}-\d{2}[ T]\d{2}:\d{2}:\d{2}(\.\d{3})?Z$'
+
+
+def del_ns(tree):
+    root = tree.getroot()
+    for elem in root.getiterator():
+        if not hasattr(elem.tag, 'find'):
+            continue
+        i = elem.tag.find('}')
+        if i >= 0:
+            elem.tag = elem.tag[i+1:]
+    lxml.objectify.deannotate(root, cleanup_namespaces=True)
+
+
+def _found(val):
+    return 'Found: %s' % val
+
+
+class TestResponse(unittest.TestCase):
+    longMessage = False
+
+    def setUp(self):
+        if not RESPONSE:
+            self.fail('RESPONSE not set')
+
+        res = None
+        with open(RESPONSE, 'rb') as f:
+            res = f.read()
+            f.close()
+
+        self.params = urllib.parse.parse_qs(
+            re.sub(r'[\s]', '', res.decode('utf-8'))
+        )
+
+        if 'SAMLResponse' not in self.params:
+            self.fail('SAMLResponse is missing')
+        if 'RelayState' not in self.params:
+            self.fail('RelayState is missing')
+
+        xml = base64.b64decode(self.params['SAMLResponse'][0])
+        self.doc = ET.parse(BytesIO(xml))
+        del_ns(self.doc)
+
+    def tearDown(self):
+        pass
+
+    def test_Response(self):
+        e = self.doc.xpath('/Response')
+        self.assertEqual(len(e), 1, 'only one Response element '
+                                    'must be present')
+
+        e = e[0]
+
+        with self.subTest('ID attribute must be present'):
+            a = e.get('ID')
+            self.assertIsNotNone(a)
+
+        with self.subTest('Version attribute must be set to 2.0'):
+            a = e.get('Version')
+            self.assertIsNotNone(a)
+            self.assertEqual(a, '2.0', _found(a))
+
+        with self.subTest('IssueInstant attribute must be '
+                          'a valid UTC string'):
+            regex = re.compile(_RE_UTC)
+            a = e.get('IssueInstant')
+            self.assertIsNotNone(a)
+            self.assertTrue(bool(regex.search(a)), _found(a))
+
+        with self.subTest('InResponseTo attribute must be present'):
+            a = e.get('InResponseTo')
+            self.assertIsNotNone(a)
+
+        with self.subTest('Destination attribute must '
+                          'be present and HTTPS URI'):
+            a = e.get('Destination')
+            self.assertIsNotNone(a)
+            self.assertTrue(a.startswith('https://'), _found(a))
+            self.assertTrue(validators.url(a), _found(a))
+
+    def test_Status(self):
+        # TODO: deal with (status_code, error_code) tuple
+
+        e = self.doc.xpath('//Response/Status')
+        self.assertEqual(len(e), 1, 'only one Status element must be present')
+
+        with self.subTest('StatusCode element must be present and valid'):
+            e = self.doc.xpath('//Response/Status/StatusCode')
+            self.assertEqual(len(e), 1, 'only one StatusCode element must '
+                                        'be present')
+
+            a = e[0].get('Value')
+            allowed_status_codes = [
+                'urn:oasis:names:tc:SAML:2.0:status:AuthnFailed',
+                'urn:oasis:names:tc:SAML:2.0:status:NoAuthnContext',
+                'urn:oasis:names:tc:SAML:2.0:status:NoPassive',
+                'urn:oasis:names:tc:SAML:2.0:status:RequestDenied',
+                'urn:oasis:names:tc:SAML:2.0:status:RequestUnsupported',
+                'urn:oasis:names:tc:SAML:2.0:status:Requester',
+                'urn:oasis:names:tc:SAML:2.0:status:Success',
+                'urn:oasis:names:tc:SAML:2.0:status:VersionMismatch',
+            ]
+            self.assertIn(a, allowed_status_codes, _found(a))
+
+        with self.subTest('StatusMessage element must be valid (if present)'):
+            e = self.doc.xpath('//Response/Status/StatusMessage')
+            self.assertLessEqual(len(e), 1, 'only one StatusMessage element '
+                                            'could be present')
+
+        with self.subTest('StatusDetail element must be valid (if present)'):
+            e = self.doc.xpath('//Response/Status/StatusDetail')
+            self.assertLessEqual(len(e), 1, 'only one StatusDetail element '
+                                            'could be present')
+
+    def test_Issuer(self):
+        e = self.doc.xpath('//Response/Issuer')
+        self.assertEqual(len(e), 1, 'Issuer element must be present')
+
+        e = e[0]
+        with self.subTest('Format attribute must be '
+                          'urn:oasis:names:tc:SAML:2.0:nameid-format:entity'):
+            a = e.get('Format')
+            self.assertIsNotNone(a)
+            self.assertEqual(
+                a,
+                'urn:oasis:names:tc:SAML:2.0:nameid-format:entity',
+                _found(a)
+            )
+
+    def test_Assertion(self):
+        # TODO: deal with "failed" responses
+
+        e = self.doc.xpath('//Response/Assertion')
+        self.assertEqual(len(e), 1, 'Assertion element must be present')
+
+        with self.subTest('AuthnStatement element must be present'):
+            e = self.doc.xpath('//Response/Assertion/AuthnStatement')
+            self.assertGreaterEqual(len(e), 1, 'At least one AuthnStatement '
+                                               'element must be present')
+
+    def test_Signature(self):
+        e = self.doc.xpath('//Response/Signature')
+        self.assertLessEqual(len(e), 1, 'Signature element could be present')
