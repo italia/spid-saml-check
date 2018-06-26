43dfb57612e71cc52c6d40aab97f39f7e80408ea
Paolo Smiraglia <paolo.smiraglia@gmail.com>
2018-06-26 08:07:50 +0200

    Define common.dump_pem module

---
+++ common/dump_pem.py
@@ -0,0 +1,39 @@
+import base64
+import re
+import OpenSSL
+
+
+def _dump_pem(xml_elem, data_dir, context, use):
+    pem = []
+    n = 78
+
+    b64 = re.sub(r'[\s]', '', xml_elem.text)
+    pem.append('-----BEGIN CERTIFICATE-----')
+    [pem.append(b64[i:i+n]) for i in range(0, len(b64), n)]
+    pem.append('-----END CERTIFICATE-----')
+
+    x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_ASN1,
+                                           base64.b64decode(b64))
+
+    dgst = x509.digest('sha256').decode('utf-8').replace(':', '')
+    fname = ('%s/%s.%s.%s.pem') % (data_dir, dgst[0:16], context, use)
+
+    with open(fname, 'w') as f:
+        f.write('\n'.join(pem))
+        f.close()
+
+
+def dump_metadata_pem(xml_elem, use, data_dir):
+    _dump_pem(xml_elem, data_dir, 'metadata', use)
+
+
+def dump_request_pem(xml_elem, use, data_dir):
+    _dump_pem(xml_elem, data_dir, 'request', use)
+
+
+def dump_response_pem(xml_elem, use, data_dir):
+    _dump_pem(xml_elem, data_dir, 'response', use)
+
+
+def dump_assertion_pem(xml_elem, use, data_dir):
+    _dump_pem(xml_elem, data_dir, 'assertion', use)
