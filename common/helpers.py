d7b8dd764c46fcdc930cec443b683f84ec2a5f8b
Paolo Smiraglia <paolo.smiraglia@gmail.com>
2018-07-10 12:37:41 +0200

    Use wrapped methods in Assertion tests

--- common/helpers.py
+++ common/helpers.py
@@ -1,4 +1,6 @@
+import base64
 import lxml.objectify
+import random
 
 
 def del_ns(tree):
@@ -17,17 +19,48 @@ def found(val):
 
 
 def dump_failures(failures):
+    who = [
+        ('IF9fX19fX19fX19fX19fX19fX19fX19fX19fX19fX19fX19fX19fX19fCi8gSGV5LC'
+         'B0aGVyZSB3YXMgYW4gZXJyb3IhIFRha2UgYSBsb29rIGluIFwKXCB0aGUgbGlzdCBi'
+         'ZWxvdy4uLiAgICAgICAgICAgICAgICAgICAgICAgLwogLS0tLS0tLS0tLS0tLS0tLS'
+         '0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0KICAgICAgICBcICAgXl9fXgogICAgICAg'
+         'ICBcICAob28pXF9fX19fX18KICAgICAgICAgICAgKF9fKVwgICAgICAgKVwvXAogIC'
+         'AgICAgICAgICAgICAgfHwtLS0tdyB8CiAgICAgICAgICAgICAgICB8fCAgICAgfHwK'),
+        ('IF9fX19fX19fX19fX19fX19fX19fX19fX19fX19fX19fX19fX19fX19fCi8gSGV5LC'
+         'B0aGVyZSB3YXMgYW4gZXJyb3IhIFRha2UgYSBsb29rIGluIFwKXCB0aGUgbGlzdCBi'
+         'ZWxvdy4uLiAgICAgICAgICAgICAgICAgICAgICAgLwogLS0tLS0tLS0tLS0tLS0tLS'
+         '0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0KICAgXAogICAgXAogICAgICAgIC4tLS4K'
+         'ICAgICAgIHxvX28gfAogICAgICAgfDpfLyB8CiAgICAgIC8vICAgXCBcCiAgICAgKH'
+         'wgICAgIHwgKQogICAgLydcXyAgIF8vYFwKICAgIFxfX18pPShfX18vCgo='),
+        ('IF9fX19fX19fX19fX19fX19fX19fX19fX19fX19fX19fX19fX19fX19fCi8gSGV5LC'
+         'B0aGVyZSB3YXMgYW4gZXJyb3IhIFRha2UgYSBsb29rIGluIFwKXCB0aGUgbGlzdCBi'
+         'ZWxvdy4uLiAgICAgICAgICAgICAgICAgICAgICAgLwogLS0tLS0tLS0tLS0tLS0tLS'
+         '0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0KICAgICAgICAgIFwKICAgICAgICAgICBc'
+         'CiAgICAgICAgICAgIFwgICAgICAgICAgX18tLS1fXwogICAgICAgICAgICAgICAgIC'
+         'AgIF8tICAgICAgIC8tLV9fX19fXwogICAgICAgICAgICAgICBfXy0tKCAvICAgICBc'
+         'IClYWFhYWFhYWFhYWFx2LgogICAgICAgICAgICAgLi1YWFgoICAgTyAgIE8gIClYWF'
+         'hYWFhYWFhYWFhYWFgtCiAgICAgICAgICAgIC9YWFgoICAgICAgIFUgICAgICkgICAg'
+         'ICAgIFhYWFhYWFhcCiAgICAgICAgICAvWFhYWFgoICAgICAgICAgICAgICApLS1fIC'
+         'BYWFhYWFhYWFhYWFwKICAgICAgICAgL1hYWFhYLyAoICAgICAgTyAgICAgKSAgIFhY'
+         'WFhYWCAgIFxYWFhYWFwKICAgICAgICAgWFhYWFgvICAgLyAgICAgICAgICAgIFhYWF'
+         'hYWCAgIFxfXyBcWFhYWFgKICAgICAgICAgWFhYWFhYX18vICAgICAgICAgIFhYWFhY'
+         'WCAgICAgICAgIFxfXy0tLS0+CiAtLS1fX18gIFhYWF9fLyAgICAgICAgICBYWFhYWF'
+         'ggICAgICBcX18gICAgICAgICAvCiAgIFwtICAtLV9fLyAgIF9fXy9cICBYWFhYWFgg'
+         'ICAgICAgICAgICAvICBfX18tLS89CiAgICBcLVwgICAgX19fLyAgICBYWFhYWFggIC'
+         'AgICAgICAgICAgICctLS0gWFhYWFhYCiAgICAgICBcLVwvWFhYXCBYWFhYWFggICAg'
+         'ICAgICAgICAgICAgICAgICAgL1hYWFhYCiAgICAgICAgIFxYWFhYWFhYWFggICBcIC'
+         'AgICAgICAgICAgICAgICAgICAvWFhYWFgvCiAgICAgICAgICBcWFhYWFhYICAgICAg'
+         'PiAgICAgICAgICAgICAgICAgXy9YWFhYWC8KICAgICAgICAgICAgXFhYWFhYLS1fXy'
+         '8gICAgICAgICAgICAgIF9fLS0gWFhYWC8KICAgICAgICAgICAgIC1YWFhYWFhYWC0t'
+         'LS0tLS0tLS0tLS0tLSAgWFhYWFhYLQogICAgICAgICAgICAgICAgXFhYWFhYWFhYWF'
+         'hYWFhYWFhYWFhYWFhYWFhYLwogICAgICAgICAgICAgICAgICAiIlZYWFhYWFhYWFhY'
+         'WFhYWFhYWFhWIiIK'),
+    ]
     return '\n'.join([
         '\n',
-        '  ________________________________________   ',
-        '/ Hey, there was a failure! Take a look in \\',
-        '\\ the list below...                        / ',
-        '  ----------------------------------------   ',
-        '         \\   ^__^                            ',
-        '          \\  (oo)\\_______                    ',
-        '             (__)\\       )\\/\\                ',
-        '                 ||----w |                   ',
-        '                 ||     ||                   ',
+        base64.b64decode(
+            who[random.randrange(0, len(who), 1)]
+        ).decode('utf-8'),
         '\n',
         '\n'.join(failures),
     ])
