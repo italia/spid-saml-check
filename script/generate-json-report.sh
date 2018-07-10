3b652cbefd692e81c7971499b95f8b5d03a896f3
Paolo Smiraglia <paolo.smiraglia@gmail.com>
2018-07-10 14:31:27 +0200

    Add environment to generate a global JSON report

--- script/generate-json-report.sh
+++ script/generate-json-report.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-jq -s '.[0] * .[1] * .[2]' ${1} ${2} ${3} > ${4}
+files=`find ${DATA_DIR} -type f -name "*.json" | tr '\n' ' '`
+IFS=' ' read -r -a array <<< ${files}
+
+for idx in ${!array[@]}; do
+    next=$(mktemp)
+    if [ ${idx} -eq 0 ]; then
+        jq -s .[0] ${array[idx]} > ${next}
+    else
+        jq -s '.[0] * .[1]' ${prev} ${array[idx]} > ${next}
+    fi
+
+    rm -f ${prev}
+    prev=${next}
+done
+
+cat ${next} > ${DATA_DIR}/report.json
