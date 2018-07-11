0e349318a63c5fbaa43b076a0bd5ebae66784ad4
Paolo Smiraglia <paolo.smiraglia@gmail.com>
2018-07-11 16:27:17 +0200

    Draft the missing test case

---
+++ test/idp/logout_response_strict.py
@@ -0,0 +1,49 @@
+import json
+import os
+import subprocess
+import unittest
+
+import common.helpers
+import common.wrap
+
+DATA_DIR = os.getenv('DATA_DIR', './data')
+
+
+class TestLogoutResponseStrict(unittest.TestCase, common.wrap.TestCaseWrap):
+    longMessage = False
+
+    @classmethod
+    def tearDownClass(cls):
+        fname = '%s/XXX.json' % DATA_DIR
+        with open(fname, 'w') as f:
+            f.write(json.dumps(cls.report, indent=2))
+            f.close()
+
+    def setUp(self):
+        self.failures = []
+        _report = self.__class__.report
+        paths = self.id().split('.')
+        c = 1
+        for path in paths:
+            if path not in _report:
+                if c == len(paths):
+                    _report[path] = {
+                        'description': self.shortDescription(),
+                        'assertions': [],
+                    }
+                else:
+                    _report[path] = {}
+            _report = _report[path]
+            c += 1
+
+    def tearDown(self):
+        if self.failures:
+            self.fail(common.helpers.dump_failures(self.failures))
+
+    def test_to_be_done(self):
+        self._assertTrue(
+            False,
+            'The test case has been implemented'
+        )
+
+# vim: ft=python
