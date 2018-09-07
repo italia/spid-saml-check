7fbc4a9aef0d9568d973d3239096f3821500be3e
Michele D'Amico <michele.damico@linfaservice.it>
2018-09-07 11:00:43 +0200

    add response 33, 34

--- spid-validator/src/views/Response/view.js
+++ spid-validator/src/views/Response/view.js
@@ -64,7 +64,10 @@ function view(me) {
                                 { value: '31a',  label: '31a. Assertion - Elemento NameID non specificato' },
                                 { value: '31b',  label: '31b. Assertion - Elemento NameID mancante' },
                                 { value: '32a',  label: '32a. Assertion - Attributo Format di NameID non specificato' },
-                                { value: '32b',  label: '32b. Assertion - Attributo Format di NameID mancante' }
+                                { value: '32b',  label: '32b. Assertion - Attributo Format di NameID mancante' },
+                                { value: '33',  label: '33. Assertion - Attributo Format di NameID diverso' },
+                                { value: '34a',  label: '34a. Assertion - Attributo NameQualifier di NameID non specificato' },
+                                { value: '34b',  label: '34b. Assertion - Attributo NameQualifier di NameID mancante' }
                             ]}>
                         </Select>
  
