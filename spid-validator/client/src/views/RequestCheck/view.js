4c50ebf4231ce28d76f741b487e319592ed708e8
damikael <michele.damico@linfaservice.it>
2020-05-06 09:45:18 +0000

    add numbers on check points

--- spid-validator/client/src/views/RequestCheck/view.js
+++ spid-validator/client/src/views/RequestCheck/view.js
@@ -6,7 +6,7 @@ import "./style.css";
 
 function view(me) { 
     return (
-        <div className="animated fadeIn">
+        <div id="RequestCheck" className="animated fadeIn"> 
             <p className="title h3">Request Report</p>
             <div className="row">
 
@@ -16,15 +16,15 @@ function view(me) {
 
                             Object.keys(me.state.result).map((t)=> {
                                 return(
-                                <div className="row testset"> 
+                                <div key={t} className="row testset"> 
                                     <div className="col-sm-12">
                                         <p><b>{me.state.test}</b> : {me.state.result[t].description}</p>
                                         
                                         {Object.keys(me.state.result[t].assertions).map((a)=> {
                                             return(
-                                                <a className={(me.state.result[t].assertions[a].result=="success")? "test-success" : "test-fail" }
+                                                <a key={a} className={(me.state.result[t].assertions[a].result=="success")? "test-success" : "test-fail" }
                                                     title={me.state.result[t].assertions[a].test +
-                                                             ": " + me.state.result[t].assertions[a].value}> &#9724; </a> 
+                                                             ": " + me.state.result[t].assertions[a].value}> {a} </a> 
                                             );
                                         })}
 
