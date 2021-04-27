5b49a5092333613680f305f8855089b9b749bb20
damikael <michele.damico@linfaservice.it>
2021-04-27 12:20:00 +0200

    fix setStore api

--- spid-validator/server/lib/database.js
+++ spid-validator/server/lib/database.js
@@ -89,19 +89,29 @@ class Database {
 
         let sql1 = "INSERT OR IGNORE INTO store(user, organization, entity_id, external_code, timestamp, type, store) VALUES (?, ?, ?, ?, DATETIME('now', 'localtime'), ?, ?);";
         let sql2 = "UPDATE store SET timestamp=DATETIME('now', 'localtime'), ";
-        if(external_code!=null && external_code!='') sql2 += "external_code=?, ";
-        sql2 += "organization=?, store=? WHERE user=? AND entity_id=? AND type=?";
 
         try { 
             store.metadata_SP_XML = utility.btoa(store.metadata_SP_XML);
-
             let storeSerialized = JSON.stringify(store);
-            this.db.prepare(sql1).run(user, organization, entity_id, external_code, store_type, storeSerialized);
+
+            let sql2_values = [];
             if(external_code!=null && external_code!='') {
-                this.db.prepare(sql2).run(external_code, organization, storeSerialized, user, entity_id, store_type);
-            } else {
-                this.db.prepare(sql2).run(organization, storeSerialized, user, entity_id, store_type);
+                sql2 += "external_code=?, ";
+                sql2_values.push(external_code);
             }
+            if(organization!=null && organization!='') {
+                sql2 += "organization=?, ";
+                sql2_values.push(organization);
+            } 
+            sql2 += "store=? WHERE user=? AND entity_id=? AND type=?";
+            sql2_values.push(storeSerialized);
+            sql2_values.push(user);
+            sql2_values.push(entity_id);
+            sql2_values.push(store_type);
+           
+            this.db.prepare(sql1).run(user, organization, entity_id, external_code, store_type, storeSerialized);
+            this.db.prepare(sql2).run(...sql2_values);
+
         } catch(exception) {
             utility.log("DATABASE EXCEPTION (saveStore)", exception.toString());
             throw(exception);
